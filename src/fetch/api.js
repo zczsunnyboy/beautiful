97b83e5f25bda9041b88a426e473a5b1b7533867
zhangchongzhou <563777343@qq.com>
2017-10-26 16:01:05 +0800

    add

---
+++ src/fetch/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+// axios 配置
+axios.defaults.timeout = 5000;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
+axios.defaults.baseURL = 'http://localhost:8080/';
+
